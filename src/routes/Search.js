a529cd23b7604860d83110db2846f03661daa97b
Ernest Viola <ernest.viol@gmail.com>
2022-08-14 15:15:42 -0400

    Added search array creation

--- src/routes/Search.js
+++ src/routes/Search.js
@@ -1,26 +1,35 @@
 import React, { useEffect, useState } from 'react'
 
+import '../css/Search.css'
+
 const Search = () => {
     const [size, setSize] = useState(0)
     const [listItems, setListItems] = useState([])
 
     const arrSizeUpdate = (e) => {
         setSize(e.target.value)
-        let a = []
-        for (let i = 0; i <= size; i++) {
-            a.push(i)
-        }
-        setListItems(a)
-        console.log(e.target.value)
+        
+        
         console.log(listItems)
     }
 
     const startSearch = (e) => {
         e.preventDefault()
+        let a = []
+        for (let i = 0; i <= size; i++) {
+            a.push(i)
+        }
+        setListItems(a)
     }
 
-
-
+    const createArray = (e) => {
+        e.preventDefault()
+        let a = []
+        for (let i = 0; i <= size; i++) {
+            a.push(i)
+        }
+        setListItems(a)
+    }
 
     return (
         <div>
@@ -28,14 +37,12 @@ const Search = () => {
             <form onSubmit={startSearch}>
                 <label>Items: </label>
                 <input min="0" max="1000" value={size} type="number" name='items' onChange={(e) => arrSizeUpdate(e)} />
-                <button >Search</button>
+                <button onClick={createArray}>Create Array</button>
+                {/* <button onClick={randomizeArray}>Randomize Array</button> */}
             </form>
-            <div>
-                {size}
-            </div>
-            <div>
+            <div className='itemContainer'>
                 {listItems.map(item =>
-                    <div key={item}>{item}</div>
+                    <div key={item} className='item'>{item}</div>
                 )}
             </div>
             
