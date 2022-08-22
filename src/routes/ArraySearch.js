7d86425dc63a06a9b9adb2392430342da38ccd4b
Ernest Viola <ernest.viol@gmail.com>
2022-08-22 11:46:37 -0400

    Added a new input field to facilitate the search function

--- src/routes/ArraySearch.js
+++ src/routes/ArraySearch.js
@@ -5,6 +5,7 @@ import '../css/Search.css'
 const ArraySearch = () => {
     const [size, setSize] = useState(0)
     const [listItems, setListItems] = useState([])
+    const [searchNumber, setSearchNumber] = useState(0)
 
     const startSearch = (e) => {
         e.preventDefault()
@@ -13,10 +14,16 @@ const ArraySearch = () => {
     const createArray = (e) => {
         e.preventDefault()
         let a = []
-        for (let i = 0; i <= size; i++) {
+        for (let i = 0; i < size; i++) {
             a.push(i)
         }
         setListItems(a)
+    }
+
+    const linearSearch = () => {
+        for (let i = 0; i <= listItems.length; i++) {
+
+        }
     }
 
     return (
@@ -26,6 +33,10 @@ const ArraySearch = () => {
                 <label>Items: </label>
                 <input min="0" max="1000" value={size} type="number" name='items' onChange={(e) => setSize(e.target.value)} />
                 <button onClick={createArray}>Create Array</button>
+                <br />
+                <label>Number to find: </label>
+                <input value={searchNumber} type="number" onChange={(e) => setSearchNumber(e.target.value)} />
+                <button>Search!</button>
                 {/* <button onClick={randomizeArray}>Randomize Array</button> */}
             </form>
             <div className='itemContainer'>
